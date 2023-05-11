6462860c608e75a694dc61da32a94d206a60c96f
Ehplodor <80175881+Ehplodor@users.noreply.github.com>
2023-05-11 23:58:28 +0200

    Update FireWire_chatGPT.r

--- FireWire/FireWire_chatGPT.r
+++ FireWire/FireWire_chatGPT.r
@@ -1,66 +1,89 @@
-# Generate random points
 N <- 1000
 X <- runif(N, -10, 10)
 Y <- runif(N, -10, 10)
 
-# Calculate distances between each pair of points
-distances <- sqrt((outer(X, X, "-")^2) + (outer(Y, Y, "-")^2))
-
-# Create a connectome based on the distances
-connectome <- matrix(0, N, N)
+distances <- matrix(0, N, N)
 for (i in 1:N) {
-  accessible <- which(distances[i, ] < 1.3 & distances[i, ] > 0)
+  for (j in 1:N) {
+    distances[i, j] <- sqrt((X[j] - X[i])^2 + (Y[j] - Y[i])^2)
+  }
+}
+
+connectome <- matrix(0, N, N)
+
+for (i in 1:N) {
+  accessible <- which(distances[i, ] < 1.3)
   if (length(accessible) > 2) {
+    accessible <- accessible[accessible != i]
     nbconnexi <- sample(2:length(accessible), 1)
     connexi <- sample(accessible, nbconnexi)
     connectome[i, connexi] <- 1
   }
 }
 
-# Add random weights to the connections
 puissance <- connectome * matrix(rnorm(N^2, 0, 1), N, N)
 
-# Visualize the network
 plot(Y ~ X)
 for (i in 1:N) {
   connectedto <- which(connectome[i, ] == 1)
   if (length(connectedto) > 0) {
-    arrows(X[i], Y[i], X[connectedto], Y[connectedto], length = 0.05,
-           lwd = abs(puissance[i, connectedto]), col = I(2 + sign(puissance[i, connectedto])))
+    arrows(x0 = rep(X[i], length(connectedto)), y0 = rep(Y[i], length(connectedto)),
+           x1 = X[connectedto], y1 = Y[connectedto],
+           length = 0.05, lwd = abs(puissance[i, connectedto]), col = I(2 + sign(puissance[i, connectedto])))
   }
 }
 
-# Select input neurons and initialize charge
-ninput <- 6
 nbconfrom <- rowSums(connectome)
-InputNeurons <- order(nbconfrom)[I(length(nbconfrom) - ninput + 1):length(nbconfrom)]
-charge <- rep(0, N)
+ninput <- 6
+InputNeurons <- tail(order(nbconfrom), ninput)
 
-# Simulate the network dynamics
+nbconto <- colSums(connectome)
+noutput <- 2
+OutputNeurons <- tail(order(nbconto), noutput)
+
+puissance2 <- puissance
+
+puissance <- puissance2
+charge <- rep(0, N)
+unavailable <- rep(0, N)
+
+windows()
+
+charge[InputNeurons] <- 10
+lastfiring <- c()
+count <- 0
 for (tps in 1:1000) {
-  # Apply input to the network
-  if (tps %% 10 == 1) charge[InputNeurons] <- 10
-  
-  # Identify firing neurons and update charge
+  count <- count + 1
+  if (count == 1) {
+    charge[InputNeurons] <- 10
+    count <- 0
+  }
+
   firing <- which(charge > 1)
   charge[firing] <- 0
-  
-  # Update the unavailable and charging neurons
-  unavailable <- ifelse(firing > 0, 5, pmax(0, unavailable - 1))
-  charging <- if (length(firing) > 1) colSums(puissance[firing, ]) else if (length(firing) == 1) puissance[firing, ] else charge * 0
+  unavailable[firing] <- 5
+  unavailable <- unavailable - 1
+  unavailable[unavailable < 0] <- 0
+  charge <- 1 * charge
+
+  if (length(firing) > 1) {
+    charging <- colSums(puissance[firing, ])
+  } else if (length(firing) == 1) {
+    charging <- puissance[firing, ]
+  } else {
+    charging <- charge * 0
+  }
   charging[unavailable > 0] <- 0
-  
-  # Update charge and visualize network activity
   charge <- charge + charging
-  fire <- rep(1, N)
-  fire[firing] <- 2
-  plot(Y ~ X, col = fire, pch = I((fire - 1) * 18) + 1)
-  
-  # Update connection weights between firing neurons and their inputs
+
   for (i in 1:length(firing)) {
-    reinforcelinkfrom <- which(connectome[, firing[i]] == 1 & lastfiring %in% InputNeurons)
+    reinforcelinkfrom <- lastfiring[connectome[lastfiring, firing[i]] == 1]
     puissance[reinforcelinkfrom, firing[i]] <- puissance[reinforcelinkfrom, firing[i]] * 1.1
   }
-  
+
   lastfiring <- firing
+  fire <- rep(1, N)
+  fire[firing] <- 2
+  plot(Y ~ X, col = fire, pch = (fire - 1) * 18 + 1)
 }
+
